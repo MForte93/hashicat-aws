806bafebc6f952d61afef8a72b9503a62c0fe30b
Martin Forte <martin.forte.93@gmail.com>
2023-06-15 21:15:59 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/hashicat-aws-forte-3/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "martinforte" 
-  versioning = {
-    enabled = true
-  }
-
 }
